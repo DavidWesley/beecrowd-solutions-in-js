2d3f1086cc056153dd0183329bf1b2119a275c5f
DavidWesley <david.wesley755@outlook.com>
2023-06-25 16:42:15 -0300

    feat: 1184 - Below the Main Diagonal

---
+++ src/beginner/1184.js
@@ -0,0 +1,54 @@
+const { readFileSync } = require("fs")
+const [CMD, ...values] = readFileSync("/dev/stdin", "utf8").split("\n")
+
+/** @param {number[]} values */
+const sumValues = ([...values], initialValue = 0) => values.reduce((acc, cur) => acc + cur, initialValue)
+
+/** @param {number[]} values */
+const mediaValues = ([...values]) => sumValues(values) / values.length
+
+/** @param {number} size*/
+const createSquareModel = (size) => ({ lengths: { cols: size, rows: size } })
+
+function createMatrixFromModel(model = createSquareModel(0), values = [], defaultReplecmentValue = 0) {
+	const { rows: rowsLenght, cols: colsLength } = model.lengths
+	const defaultRows = () => new Array(colsLength).fill(defaultReplecmentValue)
+
+	return Array.from({ length: rowsLenght }, () => {
+		return defaultRows().map((defaultValue) => values.shift() ?? defaultValue)
+	})
+}
+
+/** @typedef {'S' | 'M'} operationType */
+
+/**
+ * @param {number[][]} matrix
+ * @param {operationType} operation
+ */
+
+function selectedAreaBelowMainDiagonaToOperateFromSquareMatrix(matrix, operation) {
+	const selectedValues = []
+
+	for (let rowIndex = 0; rowIndex < matrix.length; rowIndex++)
+		for (let colIndex = 0; colIndex < rowIndex; colIndex++)
+			selectedValues.push(matrix[rowIndex][colIndex])
+
+	switch (operation) {
+	case "S": return sumValues(selectedValues)
+	case "M": return mediaValues(selectedValues)
+	default: return 0
+	}
+}
+
+
+function main() {
+	const matrixValues = values.map(Number.parseFloat)
+
+	const model = createSquareModel(12)
+	const matrix = createMatrixFromModel(model, matrixValues)
+	const response = selectedAreaBelowMainDiagonaToOperateFromSquareMatrix(matrix, (CMD === "S" ? "S" : "M"))
+
+	console.log(response.toFixed(1))
+}
+
+main()
